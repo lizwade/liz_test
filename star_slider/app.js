0c48e332518e53db57f905c955efc44bfef9e84c
Liz Wade <11866103+lizwade@users.noreply.github.com>
2024-12-14 09:38:18 +0000

    add star slider project

---
+++ star_slider/app.js
@@ -0,0 +1,24 @@
+/*
+Learning Objectives:
+  1. Manipulate DOM elements based on user input.
+  2. Dynamically create and delete elements based on slider values.
+  3. Enhance understanding of event-driven programming.
+
+Brief:
+You're provided with a slider (ranging from 1 to 5) and an empty space to populate star icons. Your task is to create or remove stars dynamically based on the value of the slider.
+
+Expected Outcomes:
+  1. The number of star icons should match the slider's value.
+  2. Adjusting the slider should immediately reflect the change in the number of star icons.
+*/
+
+// Place your plan and solution below!
+
+const slider = document.getElementById("starSlider");
+const starIcon = document.querySelector("div > span");
+
+slider.addEventListener("input", reprintStars);
+
+function reprintStars() {
+   starIcon.textContent = "★".repeat(slider.value);
+}
